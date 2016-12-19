7217216f6e2f0ed87c18d221038a0babdbcf7def
JeffBuss <turbocapslock@gmail.com>
2016-12-18 22:37:44 -0700

    Clean up structuring errors for eslintelligence and remove some unnecessary crap

--- lib/components/MessageField.jsx
+++ lib/components/MessageField.jsx
@@ -3,52 +3,51 @@ import moment from 'moment';
 
 export default class MessageField extends Component {
 
-render() {
+  render() {
+    const { messages, filteredMessages, user, reverseSort, draftMessage } = this.props;
+    let messageField;
+    if (filteredMessages.length && !reverseSort) {
+      messageField = (
+        <ul >{filteredMessages.map(m =>
+          <li key={m.key}>
+            <span>{m.createdAt}</span>
+            <span>{m.user.displayName.split(' ').shift()}</span>
+            <p>{m.content}</p>
+          </li>).reverse()}
+        </ul>);
+    } else if (filteredMessages.length) {
+      messageField = (
+        <ul >{filteredMessages.map(m =>
+          <li key={m.key}>
+            <span>{m.createdAt}</span>
+            <span>{m.user.displayName.split(' ').shift()}</span>
+            <p>{m.content}</p>
+          </li>)}
+        </ul>);
+    } else if (!reverseSort) {
+      messageField = (
+        <ul >{messages.map(m =>
+          <li key={m.key}>
+            <span>{m.createdAt}</span>
+            <span>{m.user.displayName.split(' ').shift()}</span>
+            <p>{m.content}</p>
+          </li>).reverse()}
+        </ul>);
+    } else {
+      messageField = (
+        <ul >{messages.map(m =>
+          <li key={m.key}>
+            <span>{m.createdAt}</span>
+            <span>{m.user.displayName.split(' ').shift()}</span>
+            <p>{m.content}</p>
+          </li>)}
+        </ul>);
+    }
 
-  const { messages, filteredMessages, user, reverseSort, draftMessage} = this.props;
-
-  let messageField;
-  if (filteredMessages.length && !reverseSort) {
-    messageField = (
-      <ul >{filteredMessages.map(m =>
-        <li key={m.key}>
-          <span>{m.createdAt}</span>
-          <span>{m.user.displayName.split(' ').shift()}</span>
-          <p>{m.content }</p>
-        </li>).reverse()}
-      </ul>);
-  } else if (filteredMessages.length) {
-    messageField = (
-      <ul >{filteredMessages.map(m =>
-        <li key={m.key}>
-          <span>{m.createdAt}</span>
-          <span>{m.user.displayName.split(' ').shift()}</span>
-          <p>{m.content }</p>
-        </li>)}
-      </ul>);
-  } else if (!reverseSort) {
-    messageField = (
-      <ul >{messages.map(m =>
-        <li key={m.key}>
-          <span>{m.createdAt}</span>
-          <span>{m.user.displayName.split(' ').shift()}</span>
-          <p>{m.content }</p>
-        </li>).reverse()}
-      </ul>);
-  } else {
-    messageField = (
-      <ul >{messages.map(m =>
-        <li key={m.key}>
-          <span>{m.createdAt}</span>
-          <span>{m.user.displayName.split(' ').shift()}</span>
-          <p>{m.content }</p>
-        </li>)}
-      </ul>);
+    return (
+      <div>
+        {messageField}
+      </div>
+    );
   }
-
-  return (
-    <div>
-      {messageField}
-    </div>
-  )};
 }
